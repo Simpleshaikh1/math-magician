e1007465acd109472ec1cc31921050066fa071d3
Simpleshaikh1 <Hamzatbolaji648@yahoo.com>
2023-06-15 18:13:16 +0100

    All Operation implemented

--- src/Components/calculator.js
+++ src/Components/calculator.js
@@ -1,10 +1,27 @@
+import { useState } from 'react';
+import calculate from '../Logic/calculate';
+
 function Calculator() {
+  const [buttonItem, setbuttonItem] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const clickButtonHandler = (event) => {
+    const buttonName = event.target.innerText;
+    setbuttonItem(calculate(buttonItem, buttonName));
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  const { total, next, operation } = buttonItem;
+
   const createDigits = () => {
     const digits = [];
 
     for (let i = 9; i >= 1; i -= 1) {
       digits.push(
-        <button type="button" key={i}>{i}</button>,
+        <button type="button" onClick={clickButtonHandler} key={i}>{i}</button>,
       );
     }
 
@@ -15,24 +32,77 @@ function Calculator() {
     <div className="App">
       <div className="calculator">
         <div className="display">
-          0
+          <span>{next || operation}</span>
+          {next || total || '0'}
         </div>
         <div className="ops-dig-container">
           <div className="operators">
-            <button type="button">÷</button>
-            <button type="button">x</button>
-            <button type="button">+</button>
-            <button type="button">-</button>
-            <button type="button">=</button>
+            <button
+              type="button"
+              onClick={clickButtonHandler}
+            >
+              ÷
+            </button>
+            <button
+              type="button"
+              onClick={clickButtonHandler}
+            >
+              x
+            </button>
+            <button
+              type="button"
+              onClick={clickButtonHandler}
+            >
+              +
+            </button>
+            <button
+              type="button"
+              onClick={clickButtonHandler}
+            >
+              -
+            </button>
+            <button
+              type="button"
+              onClick={clickButtonHandler}
+            >
+              =
+            </button>
           </div>
 
           <div className="digits">
-            <button type="button">%</button>
-            <button type="button">+/-</button>
-            <button type="button">AC</button>
+            <button
+              type="button"
+              onClick={clickButtonHandler}
+            >
+              %
+            </button>
+            <button
+              type="button"
+              onClick={clickButtonHandler}
+            >
+              +/-
+            </button>
+            <button
+              type="button"
+              onClick={clickButtonHandler}
+            >
+              AC
+            </button>
             {createDigits()}
-            <button type="button" className="dot">.</button>
-            <button type="button" className="zero">0</button>
+            <button
+              type="button"
+              className="dot"
+              onClick={clickButtonHandler}
+            >
+              .
+            </button>
+            <button
+              type="button"
+              className="zero"
+              onClick={clickButtonHandler}
+            >
+              0
+            </button>
           </div>
         </div>
       </div>
