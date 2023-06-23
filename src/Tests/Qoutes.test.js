b81441a3626d5ead3ab4bad2c1b6f6add4ba7e1e
Simpleshaikh1 <Hamzatbolaji648@yahoo.com>
2023-06-23 08:45:40 +0100

    here

--- src/Tests/Qoutes.test.js
+++ src/Tests/Qoutes.test.js
@@ -1,7 +1,2 @@
 import { render } from '@testing-library/react';
 import Quotes from '../Routes/displayQoute';
-
-test('renders Quotes correctly', () => {
-  const { container } = render(<Quotes />);
-  expect(container).toMatchSnapshot();
-});
